09c417d1b01e262d69f7a5cdb578016189d26871
chaadow <chaadow@msn.com>
2025-01-06 19:51:37 +0100

    Fix

--- tmux/config.zsh
+++ tmux/config.zsh
@@ -13,29 +13,6 @@ ensure_tmux_is_running() {
   fi
 }
 
-_cdpath_directories() {
-  modified_in_last_days=${1:-999}
-  echo "${CDPATH//:/\n}" | while read dir; do
-    find -L "$dir" \
-      -not -path '*/\.*' \
-      -type d \
-      -atime -"$modified_in_last_days" \
-      -maxdepth 5
-  done
-}
-
-_is_a_git_repo() {
-  while read dir; do
-    if [[ -d "$dir/.git" ]]; then
-      basename "$dir"
-    fi
-  done
-}
-
-projects() {
-  _cdpath_directories $1 | _is_a_git_repo
-}
-
 # Experimental
 # From wincent : https://github.com/wincent/wincent/roles/dotfiles/files/.zsh/functions#L41-L80
 function tmux() {
